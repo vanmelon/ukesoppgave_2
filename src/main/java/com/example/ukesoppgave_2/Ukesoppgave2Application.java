2d01f8eb521159a54995350395ee3277fdea401f
vanmelon <hms2006@online.no>
2023-12-10 15:39:28 +0100

    lager reposetory

---
+++ src/main/java/com/example/ukesoppgave_2/Ukesoppgave2Application.java
@@ -0,0 +1,13 @@
+package com.example.ukesoppgave_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ukesoppgave2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ukesoppgave2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ukesoppgave_2/Ukesoppgave2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukesoppgave_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ukesoppgave2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
