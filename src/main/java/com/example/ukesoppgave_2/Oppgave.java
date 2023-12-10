88cf96a56aaa20478643c4ef0460081de6b3af5a
vanmelon <hms2006@online.no>
2023-12-10 15:46:55 +0100

    leger til java koden

--- src/main/java/com/example/ukesoppgave_2/Oppgave.java
+++ src/main/java/com/example/ukesoppgave_2/Oppgave.java
@@ -1,4 +1,17 @@
 package com.example.ukesoppgave_2;
 
 public class Oppgave {
+    private String tittel;
+    private String beskrivelse;
+
+    // Konstruktør, gettere og settere her
+
+    // Eksempel på getter:
+    public String getTittel() {
+        return tittel;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
 }
--- src/main/java/com/example/ukesoppgave_2/OppgaveController.java
+++ src/main/java/com/example/ukesoppgave_2/OppgaveController.java
@@ -1,4 +1,16 @@
 package com.example.ukesoppgave_2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OppgaveController {
+    private List<Oppgave> oppgaver = new ArrayList<>();
+
+    public void leggTilOppgave(Oppgave oppgave) {
+        oppgaver.add(oppgave);
+    }
+
+    public List<Oppgave> hentAlleOppgaver() {
+        return oppgaver;
+    }
 }
